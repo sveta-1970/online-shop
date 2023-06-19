c241e58d3fbea3bdc8bd526cfbda34cfd246274d
sveta-1970 <ssarisoydan@gmail.com>
2023-06-19 15:26:20 +0300

    Using index.js added cards to catalog page

--- code/index.js
+++ code/index.js
@@ -3,3 +3,24 @@
 if (!Array.isArray(data)) {
   throw new Error(`Ми отримали щось інше...`);
 }
+
+data.forEach((obj) => {
+  const card = `<div class="card">
+                <img
+                  class="product_image"
+                  src="${obj.image1}"
+                  alt="${obj.type}"
+                />
+                <h5>${obj.description}</h4>
+                <div class="stars">
+                  <img src="./images/homepage_images/Star 1.png" alt="star" />
+                  <img src="./images/homepage_images/Star 1.png" alt="star" />
+                  <img src="./images/homepage_images/Star 1.png" alt="star" />
+                  <img src="./images/homepage_images/Star 1.png" alt="star" />
+                  <img src="./images/homepage_images/Star 1.png" alt="star" />
+                </div>
+                <p><span>${obj.price} hrv</span></p>
+                <button>Add to cart</button>
+              </div>`;
+  document.getElementById("catalog-cards").innerHTML += card;
+});
