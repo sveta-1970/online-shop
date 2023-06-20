95040a38b7fee0cdd3812f19301bae9ffd653f93
sveta-1970 <ssarisoydan@gmail.com>
2023-06-20 13:23:08 +0300

    Styled images for subnails in modal window

--- code/index.js
+++ code/index.js
@@ -5,11 +5,17 @@ if (!Array.isArray(data)) {
 }
 
 //вивід інфо на карточки на сторінці catalog
+
 data.forEach((obj) => {
   const card = `<div class="card">
                 <img
                   class="product_image"
                   src="${obj.image1}"
+                  alt="${obj.type}"
+                />
+                <img
+                  class="product_image2"
+                  src="${obj.image2}"
                   alt="${obj.type}"
                 />
                 <h5>${obj.description}</h4>
@@ -34,9 +40,27 @@ data.forEach((obj) => {
   }
 });
 
+//catalog page, aside-container => product
 const sorted = products.toSorted();
 sorted.forEach((el) => {
   document.getElementsByClassName(
     ".dropdownlist.product div.checkbox"
   ).innerHTML = `${sorted[el]}`;
 });
+
+//modal window
+function showModal() {
+  //головне зображення
+  const main_image = document.getElementsByClassName("main-image");
+  main_image = document.getElementsByClassName("product_image");
+  
+  //миниатюрне зображення
+  const subnail_image = document.getElementsByClassName("subnail-image");
+  subnail_image = main_image.classList.add("subnail-image");
+  main_image.classList.remove("subnail-image");
+
+
+}
+
+const catalog_card = document.getElementsByClassName("card");
+catalog_card.addEventListener("click", showModal);
